dd72f5e29c24c2194694b88613739731084abf22
Jamie Wales <jamie.wales.developer@gmail.com>
2021-01-18 17:13:15 +0000

    final changes adding lazyloading

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+//header
 const burgerBtn = document.getElementsByClassName('navigation__burger-menu');
 const menu = document.querySelector('.navigation__mobile');
 const logo = document.querySelector('.logo')
@@ -21,3 +23,52 @@ burgerBtn[0].addEventListener('click', () => {
 
 });
 
+//lazy-loading CSS background of our-creations section
+document.addEventListener("DOMContentLoaded", () => {
+    let lazyloadImages;    
+  
+    if ("IntersectionObserver" in window) {
+      lazyloadImages = document.querySelectorAll(".lazy");
+      const imageObserver = new IntersectionObserver((entries, observer) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            let image = entry.target;
+            image.classList.remove("lazy");
+            imageObserver.unobserve(image);
+          }
+        });
+      });
+  
+      lazyloadImages.forEach((image) => {
+        imageObserver.observe(image);
+      });
+    } else {  
+      let lazyloadThrottleTimeout;
+      lazyloadImages = document.querySelectorAll(".lazy");
+      
+      const lazyload = () => {
+        if(lazyloadThrottleTimeout) {
+          clearTimeout(lazyloadThrottleTimeout);
+        }    
+  
+        lazyloadThrottleTimeout = setTimeout( () => {
+          let scrollTop = window.pageYOffset;
+          lazyloadImages.forEach(img => {
+              if(img.offsetTop < (window.innerHeight + scrollTop)) {
+                img.classList.remove('lazy');
+              }
+          });
+          if(lazyloadImages.length == 0) { 
+            document.removeEventListener("scroll", lazyload);
+            window.removeEventListener("resize", lazyload);
+            window.removeEventListener("orientationChange", lazyload);
+          }
+        }, 100);
+      }
+  
+      document.addEventListener("scroll", lazyload);
+      window.addEventListener("resize", lazyload);
+      window.addEventListener("orientationChange", lazyload);
+    }
+  })
+  
